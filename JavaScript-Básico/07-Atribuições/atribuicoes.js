d7365d54798bc4af7d81f5b59eb83c8a912aea0a
Pedro Alquimim <89994181+pedrOAlquimim@users.noreply.github.com>
2021-12-02 20:34:43 -0300

    Upload files

--- JavaScript-Básico/07-Atribuições/atribuicoes.js
+++ JavaScript-Básico/07-Atribuições/atribuicoes.js
@@ -5,4 +5,21 @@
 // %=   x %= y   x = x % 1
 
 var a = 5;
+a += 3;
 console.log(a);
+
+var b = 4;
+b -= 3;
+console.log(b);
+
+var c = 2;
+c *= 4;
+console.log(c);
+
+var d = 10;
+d /= 5;
+console.log(d);
+
+var e = 13;
+e %= 7;
+console.log(e);
---
+++ JavaScript-Básico/08-Operadores_Comparativos/comparativos.js
@@ -0,0 +1,24 @@
+// == Igual a
+// === Valor igual e igual
+// != Diferente
+// !== Não igual ou não igual
+// > Maior que
+// < Menor que
+// >= Maior ou igual a
+// <= Menor ou igual a
+
+var a = 3;
+var b = "3";
+console.log(a == b);
+console.log(a === b);
+console.log(a != b);
+console.log(a !== b);
+
+console.log("-----------------------------------");
+
+var c = 5;
+var d = 5;
+console.log(c > d);
+console.log(c < d);
+console.log(c >= d);
+console.log(c <= d);
