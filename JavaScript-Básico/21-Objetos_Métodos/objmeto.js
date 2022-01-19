032dd69cfc90d91a9d280d4f9aa46c4ca061fbf1
Pedro Alquimim <89994181+pedrOAlquimim@users.noreply.github.com>
2022-01-19 18:35:24 -0300

    Upload files

--- JavaScript-Básico/21-Objetos_Métodos/objmeto.js
+++ JavaScript-Básico/21-Objetos_Métodos/objmeto.js
@@ -1,13 +1,22 @@
- function mediaAluno() {
-    return (this.grades[0] + this.grades[1]) / 2;
+function mediaAluno() {
+  return (this.grades[0] + this.grades[1]) / 2;
 
- }
+}
  
- var aluno = {
-   name: "Bruno",
-   grades: [7, 9],
+var aluno = {
+  name: "Bruno",
+  grades: [7, 9],
 
-   media: mediaAluno
- }
+  media: mediaAluno
+}
+
+var aluno1 = {
+  name: "Afonso",
+  grades: [10, 2],
+
+  media: mediaAluno
+
+}
 
  console.log(aluno.media());
+ console.log(aluno1.media()); 
