72ad5c09a8064f79a89ae2a4012e70ca270836a1
Pedro Alquimim <89994181+pedrOAlquimim@users.noreply.github.com>
2022-01-26 19:09:15 -0300

    Upload files

--- JavaScript-Intermediário/5-Acessando_elementos_HTML_com_JS/htmlcomjs.js
+++ JavaScript-Intermediário/5-Acessando_elementos_HTML_com_JS/htmlcomjs.js
@@ -2,12 +2,12 @@ let per = document.getElementById("p1");
 //console.log(per);
 
 let par = document.getElementsByClassName("para");
-//console.log(par);
+console.log(par);
 
-// for (let p of par) {
-//     console.log(p);
+for (let p of par) {
+    console.log(p);
 
-// }
+}
 
 let th = document.getElementsByTagName("p");
 console.log(th[0]);
