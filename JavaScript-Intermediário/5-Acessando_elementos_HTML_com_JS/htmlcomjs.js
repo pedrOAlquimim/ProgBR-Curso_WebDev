ead0879a4c039648630f595b7c02da7de0644340
Pedro Alquimim <89994181+pedrOAlquimim@users.noreply.github.com>
2022-01-26 19:05:53 -0300

    Uload files

---
+++ JavaScript-Intermediário/5-Acessando_elementos_HTML_com_JS/htmlcomjs.js
@@ -0,0 +1,13 @@
+let per = document.getElementById("p1");
+//console.log(per);
+
+let par = document.getElementsByClassName("para");
+//console.log(par);
+
+// for (let p of par) {
+//     console.log(p);
+
+// }
+
+let th = document.getElementsByTagName("p");
+console.log(th[0]);
